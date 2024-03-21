7612e9be307dd3783b443ba76b40165fd1cb7644
Fei Xie <fionaxiework@gmail.com>
2024-03-20 21:42:24 -0400

    initail js script

---
+++ script.js
@@ -0,0 +1,10 @@
+searchButton = document.querySelector('button')
+searchInput = document.querySelector('input')
+
+function searchQuery(){
+    searchButton.addEventListener('click', function(){
+        const query = searchInput.value
+        window
+}}
+
+searchButton.addEventListener('click', searchQuery)
