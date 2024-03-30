6618362f5951be9e97c6b7de303a5e8a9b947876
Fei Xie <fionaxiework@gmail.com>
2024-03-29 20:25:40 -0400

    side window

--- script.js
+++ script.js
@@ -10,8 +10,14 @@ function searchQuery(){
         .then((response) => {
             if (prevSearch){
                 document.querySelector('#results').innerHTML = ''
+                prevSearch = false
+            }
+            if(sideWindow){
+                document.querySelector('.moreInfo').remove()
+                sideWindow = false
             }
             prevSearch = true
+            document.querySelector('#result-box').style.borderStyle = 'dashed';
             console.log(response.data)
             for (let i = 0; i < response.data.length; i++){
                 const show = response.data[i].show
@@ -30,24 +36,28 @@ function searchQuery(){
 }
 
 function moreInfo(showID){
+    if (sideWindow){
+        document.querySelector('.moreInfo').remove()
+    }
     var name, rating, summary, image;
     searchResults = axios.get(`https://api.tvmaze.com/shows/${showID}`)
     .then((response) => {
         console.log(response)
         sideWindow = true;
         name = response.data.name;
-        rating = response.data.rating.average;
         summary = response.data.summary;
         image = response.data.image.medium;
+        moreInfoWindow = document.createElement('div')
+        moreInfoWindow.id = 'moreInfo'
+        moreInfoWindow.innerHTML = `<h2>${name}</h2>
+                                
+                                    <img src="${image}">
+                                    <p>${summary}</p>`
+        moreInfoWindow.classList.add('moreInfo')
+        document.querySelector('#result-box').appendChild(moreInfoWindow)
     })
 
-    moreInfoWindow = document.createElement('div')
-    moreInfoWindow.id = 'moreInfo'
-    moreInfoWindow.innerHTML = `<h2>${name}</h2>
-                                <p>${rating}</p>
-                                <img src="${image}">
-                                <p>${summary}</p>`
-    document.querySelector('#result-box').appendChild(moreInfoWindow)
+    
 }
 //searching for shows 
 searchButton.addEventListener('click', searchQuery)
